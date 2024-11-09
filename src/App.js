d27bfc23e4764b5244c881d58ac9a5bc2fd0655e
Alethia Quintero <alethiaq22@gmail.com>
2024-11-09 18:26:30 -0500

    add bootrap style

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+// import './App.css';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid>
+      <Row>
+        <Card bg={'primary'} style={{ width: '18rem'}} >
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Row>
+    </Container>
   );
 }
 
 export default App;
+{/* <Card bg={'danger'} border="dark" style={{ width: '18rem' }}>
+<Card.Body>
+ <Card.Title>Card Title</Card.Title>
+ <Card.Text>
+   Some quick example text to build on the card title and make up the
+   bulk of the card's content.
+ </Card.Text>
+</Card.Body>
+</Card> */}
