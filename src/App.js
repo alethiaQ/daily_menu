378bc4383ac0c1cddbfad6d4d3045750590d52a2
Alethia Quintero <alethiaq22@gmail.com>
2024-11-23 17:59:45 -0500

    remove old css, add class

--- src/App.js
+++ src/App.js
@@ -6,17 +6,9 @@ import Col from 'react-bootstrap/Col';
 function App() {
   return (
     <Container fluid>
-      <Row>
-        <Card bg={'primary'} style={{ width: '18rem'}} >
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </Row>
+      <Card className='menu_item_title'>
+        YUMMY JUICE
+      </Card>
     </Container>
   );
 }
