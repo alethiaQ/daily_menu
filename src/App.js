c0afef4c74654dc36dac3a7b56423a5979a235d7
Alethia Quintero <alethiaq22@gmail.com>
2024-11-23 18:12:17 -0500

    update card

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import './App.css';
+import './App.css';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -6,20 +6,17 @@ import Col from 'react-bootstrap/Col';
 function App() {
   return (
     <Container fluid>
-      <Card className='menu_item_title'>
-        YUMMY JUICE
+      <Card>
+        <Card.Title className='menu_item_title'>
+          YUMMY JUICE
+        </Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
       </Card>
     </Container>
   );
 }
 
 export default App;
-{/* <Card bg={'danger'} border="dark" style={{ width: '18rem' }}>
-<Card.Body>
- <Card.Title>Card Title</Card.Title>
- <Card.Text>
-   Some quick example text to build on the card title and make up the
-   bulk of the card's content.
- </Card.Text>
-</Card.Body>
-</Card> */}
