28a82503ae4732cdc0e77ac8af08e0540f927ad2
Alethia Quintero <alethiaq22@gmail.com>
2024-11-23 22:08:11 -0500

    Responsive Layout for content section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 function App() {
   return (
     <Container fluid className="mt-4">
       <Row className="justify-content-center menu_item_title_var2" style={{ fontSize: '100px' }}>
         Recipe Title
       </Row>
-      <Row>
-        Content Row 
+      <Row className="p-4">
+        <Col lg={4}>Left Side Container</Col>
+        <Col>Right Side Container</Col>
       </Row>
     </Container>
   );
