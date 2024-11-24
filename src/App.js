3bbf43cf89a9c075efdb4cd0d649fd47c5861382
Alethia Quintero <alethiaq22@gmail.com>
2024-11-23 21:39:31 -0500

    Add Title text, remove unnecessary comp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row'
 function App() {
   return (
     <Container fluid className="mt-4">
-      <Row className="justify-content-center">
-        Header Row
+      <Row className="justify-content-center menu_item_title_var2" style={{ fontSize: '100px' }}>
+        Recipe Title
       </Row>
       <Row>
         Content Row 
@@ -16,12 +15,3 @@ function App() {
 }
 
 export default App;
-{/* <Card className='d-flex justify-content-center'>
-        <Card.Title className='menu_item_title_var2' style={{ fontSize: '100px' }}>
-          Yummy Juice
-        </Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-      </Card> */}
