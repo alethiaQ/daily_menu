2c8d0fc0c88d6c0e562d3ba0e142a0b3d96f043b
Alethia Quintero <alethiaq22@gmail.com>
2024-11-23 22:21:02 -0500

    add recipe colors

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 function App() {
   return (
-    <Container fluid className="mt-4">
+    <Container fluid className="recipe_text_color recipe_background_color">
       <Row className="justify-content-center menu_item_title_var2" style={{ fontSize: '100px' }}>
-        Recipe Title
+        Yummy Juice
       </Row>
       <Row className="p-4">
         <Col lg={4}>Left Side Container</Col>
