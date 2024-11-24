d6e4d0175d454279ec22d624ba059064e26fc448
Alethia Quintero <alethiaq22@gmail.com>
2024-11-24 10:22:32 -0500

    setup ingredient list component and pass props

---
+++ src/components/IngredientsList.js
@@ -0,0 +1,10 @@
+import { Card } from "react-bootstrap"
+
+
+export const IngredientList = ({ ingredients }) => {
+    return (
+        <Card>
+            Ingredient List 
+        </Card>
+    )
+}
--- src/containers/LeftSideContainer.js
+++ src/containers/LeftSideContainer.js
@@ -1,10 +1,13 @@
 import { InfoCard } from "../components/InfoCard";
+import { IngredientList } from "../components/IngredientsList";
 
 export function LeftSideContainer() {
-    let info = {makes: 2, goes_with: ["put the lime in the coconut bread with salty coconut jam (page132)", "Party Chix (Page 185)"]}
+    const info = { makes: 2, goes_with: ["put the lime in the coconut bread with salty coconut jam (page132)", "Party Chix (Page 185)"] };
+    const ingredients = ["2 lemons", "Ice", "2 ounces Aperol or Cappelletti", "4 ounces mezcal", "3/4 ounce agave syrup", "Kosher salt"];
     return ( 
         <span>
             <InfoCard info={info} />
+            <IngredientList ingredients={ingredients} />
         </span>
     )
 }
