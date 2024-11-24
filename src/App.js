e1b166469a7a3d66db2218a633699ec83901036c
Alethia Quintero <alethiaq22@gmail.com>
2024-11-23 20:48:25 -0500

    pre phase 1 of responsive and grid layout setup / testing font

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 function App() {
   return (
     <Container fluid>
-      <Card>
-        <Card.Title className='menu_item_title'>
-          YUMMY JUICE
+      <Card className='d-flex justify-content-center'>
+        <Card.Title className='menu_item_title_var2' style={{ fontSize: '100px' }}>
+          Yummy Juice
         </Card.Title>
         <Card.Text>
           Some quick example text to build on the card title and make up the
