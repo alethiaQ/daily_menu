b6f64af79fd7ceba939b1dcd53afa8493b50b4bc
Alethia Quintero <alethiaq22@gmail.com>
2024-11-23 21:20:10 -0500

    add rows

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row'
 function App() {
   return (
-    <Container fluid>
-      <Card className='d-flex justify-content-center'>
+    <Container fluid className="mt-4">
+      <Row className="justify-content-center">
+        Header Row
+      </Row>
+      <Row>
+        Content Row 
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
+{/* <Card className='d-flex justify-content-center'>
         <Card.Title className='menu_item_title_var2' style={{ fontSize: '100px' }}>
           Yummy Juice
         </Card.Title>
@@ -12,9 +24,4 @@ function App() {
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text>
-      </Card>
-    </Container>
-  );
-}
-
-export default App;
+      </Card> */}
